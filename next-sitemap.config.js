4c99791f90a3e770ee9f92e30d60f1773c21185a
vishak <vishakvenu1998@gmail.com>
2024-08-17 07:43:39 +0530

    site-maps added

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://www.starshineinterlockingbricks.com', // Replace with your website URL
+  generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+  sitemapSize: 7000, // (Optional) Limit the number of URLs per sitemap file
+  changefreq: 'daily',
+  priority: 0.7,
+};
